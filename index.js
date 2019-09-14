8f914a4451ef88242816e5bfe5173e2f42646e2a
mdmnk <mdmnk@150ml.com>
2019-09-14 03:41:56 -0600

    Reduces code usage

--- index.js
+++ index.js
@@ -4,46 +4,44 @@ class Cipher {
   constructor(secret)
   {
     this.alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const keywrd = this.parse(secret).split('').filter(this.distinct).join('');
-    const cipher = keywrd.split('').sort().join('');
-    this.width = cipher.length;
-    this.height = Math.ceil(this.alpha.length / this.width);
+    const key = this._distinct(this._parse(secret));
+    const cipher = key.split('').sort();
     this.trans = [];
-    this.kwAlpha = (keywrd + this.alpha).split('').filter(this.distinct).join('') + Array(((this.width * this.height) - this.alpha.length)).join(' ');
-    cipher.split('').forEach((_, i) => this.trans[cipher.indexOf(this.kwAlpha[i])] = cipher.indexOf(this.kwAlpha[i]) - i);
-    this.encryptedAlpha = this.parse(this.kwAlpha.split('').map((_, i) => this.kwAlpha[this.transIndex(i)]).join(''));
+    this.width = cipher.length;
+    this.height = Math.ceil(this.alpha.length / cipher.length);
+    this.kAlpha = this._distinct(key + this.alpha) + Array(this.width * this.height - this.alpha.length).join(' ');
+    cipher.forEach((_, i) => this.trans[cipher.indexOf(this.kAlpha[i])] = cipher.indexOf(this.kAlpha[i]) - i);
+    this.encryptedAlpha = this._parse(this.kAlpha.split('').map((_, i) => this.kAlpha[
+      this._max(this._flatIndex(i) - this.trans[this._flatIndex(i) % this.width], this.kAlpha.length)
+    ]).join(''));
+  }
+
+  _distinct(val) {
+    return val.split('').filter((item, i, self) => self.indexOf(item) === i).join('');
   }
   
-  distinct(val, index, self) {
-    return self.indexOf(val) === index;
-  }
-  
-  max(i, max) {
+  _max(i, max) {
     return (i >= max) ? max - 1 : i;
   }
   
-  flattenIndex(i) {
+  _flatIndex(i) {
     return Math.floor(i / this.height) + (i % this.height) * this.width;
   }
 
-  transIndex(i) {
-    return this.max(this.flattenIndex(i) - this.trans[this.flattenIndex(i) % this.width], this.kwAlpha.length);
-  }
-
-  crypt(s, alphaA, alphaB) {
+  _crypt(s, alphaA, alphaB) {
     return s.split('').map(lttr => alphaA[alphaB.indexOf(lttr)]).join('').replace(/.{5}/g, '$& ');
   }
   
+  _parse(s) {
+    return s.toUpperCase().replace(/ /g, '').replace(/[^a-zA-Z]/g, '');
+  }
+
   encrypt(s) {
-    return this.crypt(this.parse(s), this.encryptedAlpha, this.alpha);
+    return this._crypt(this._parse(s), this.encryptedAlpha, this.alpha);
   }
   
   decrypt(s) {
-    return this.crypt(this.parse(s), this.alpha, this.encryptedAlpha);
-  }
-  
-  parse(s) {
-    return s.toUpperCase().replace(/ /g, '').replace(/[^a-zA-Z]/g, '');
+    return this._crypt(this._parse(s), this.alpha, this.encryptedAlpha);
   }
 }
 
