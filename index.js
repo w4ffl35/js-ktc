c7e04b2482bbe715e97472768d1cfef635ffd842
mdmnk <mdmnk@150ml.com>
2019-09-13 21:31:11 -0600

    Reduces code

--- index.js
+++ index.js
@@ -1,74 +1,50 @@
-"use strict";
+'use strict';
 
 class Cipher {
-    constructor(secret)
-    {
-        this.alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        this.indexTransformations = [];
-        this.keyword = '';
-        this.kwAlpha = '';
-        this.encryptedAlpha = '';
-        this.cipher = '';
-        secret = this.parseString(secret);
-        if (secret === '') return;
-        this.distinct(secret, this, 'keyword');
-        this.cipher = this.keyword.split('').sort().join('');
-        this.width = this.cipher.length;
-        this.height = Math.ceil(26.0 / this.width);
-        this.indexTransformations = [];
-        this.distinct(this.keyword + this.alpha, this, 'kwAlpha');
-        for (let i = 0; i < ((this.width * this.height) - this.alpha.length); i++) this.kwAlpha += ' ';
-        this.setIndexTransformations();
-        this.setEncryptedAlpha();
-    }
+  constructor(secret)
+  {
+    this.alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const keywrd = this.parse(secret).split('').filter(this.distinct).join('');
+    const cipher = keywrd.split('').sort().join('');
+    this.width = cipher.length;
+    this.height = Math.ceil(this.alpha.length / this.width);
+    this.trans = [];
+    this.kwAlpha = (keywrd + this.alpha).split('').filter(this.distinct).join('') + Array(((this.width * this.height) - this.alpha.length)).join(' ');
+    cipher.split('').forEach((_, i) => this.trans[cipher.indexOf(this.kwAlpha[i])] = cipher.indexOf(this.kwAlpha[i]) - i);
+    this.encryptedAlpha = this.parse(this.kwAlpha.split('').map((_, i) => this.kwAlpha[this.transIndex(i)]).join(''));
+  }
+  
+  distinct(val, index, self) {
+    return self.indexOf(val) === index;
+  }
+  
+  max(i, max) {
+    return (i >= max) ? max - 1 : i;
+  }
+  
+  flattenIndex(i) {
+    return Math.floor(i / this.height) + (i % this.height) * this.width;
+  }
 
-    distinct(keyword, obj, prop) {
-        keyword.split('').map((item) => {
-            return (obj[prop].indexOf(item) === -1) ? obj[prop] += item : false;
-        });
-    }
+  transIndex(i) {
+    return this.max(this.flattenIndex(i) - this.trans[this.flattenIndex(i) % this.width], this.kwAlpha.length);
+  }
 
-    setIndexTransformations()
-    {
-        for (let i = 0; i < this.width; i++) {
-            let indexDist = this.cipher.indexOf(this.kwAlpha[i]) - i;
-            this.indexTransformations[i + (indexDist)] = indexDist;
-        }
-    }
-
-    setEncryptedAlpha()
-    {
-        let e = "";
-        for (let i=0; i < this.kwAlpha.length; i++) {
-            let index = Math.floor(i / this.height) + (i % this.height) * this.width;
-            index -= this.indexTransformations[index % this.width];
-            index = (index >= this.kwAlpha.length) ? this.kwAlpha.length - 1 : index;
-            e += this.kwAlpha[index];
-        }
-        this.encryptedAlpha = this.parseString(e);
-    }
-
-    encrypt(s)
-    {
-        return this.crypt(this.parseString(s), this.encryptedAlpha, this.alpha);
-    }
-
-    decrypt(s)
-    {
-        return this.crypt(this.parseString(s), this.alpha, this.encryptedAlpha);
-    }
-
-    crypt(s, alphaA, alphaB)
-    {
-        if (this.keyword === '') return '';
-        let e = '';
-        for (let i = 0; i < s.length; i++) e += alphaA[alphaB.indexOf(s[i])];
-        return e.replace(/.{5}/g, '$& ');
-    }
-
-    parseString(s) {
-        return s.toUpperCase().replace(/ /g, '').replace(/[^a-zA-Z]/g, '');
-    }
+  crypt(s, alphaA, alphaB) {
+    return s.split('').map(lttr => alphaA[alphaB.indexOf(lttr)]).join('').replace(/.{5}/g, '$& ');
+  }
+  
+  encrypt(s) {
+    return this.crypt(this.parse(s), this.encryptedAlpha, this.alpha);
+  }
+  
+  decrypt(s) {
+    return this.crypt(this.parse(s), this.alpha, this.encryptedAlpha);
+  }
+  
+  parse(s) {
+    return s.toUpperCase().replace(/ /g, '').replace(/[^a-zA-Z]/g, '');
+  }
 }
 
-module.exports = { Cipher };
+module.exports = Cipher;
