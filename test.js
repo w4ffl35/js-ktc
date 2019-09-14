b859874a8010b098d40ea29b48479ec8f6cf0259
mdmnk <mdmnk@150ml.com>
2019-09-13 21:30:39 -0600

    Fixes test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const KTC = require('js-keyword-transposition-cipher');
-let ktc = new KTC.Cipher("secret");
+const Cipher = require('./index');
+let ktc = new Cipher("secret");
 const encrypted = ktc.encrypt("cryptology");
 const decrypted = ktc.decrypt(encrypted);
 console.log(encrypted);
